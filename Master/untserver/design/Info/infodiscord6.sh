c2a740860a1b847e90ef05eaee2047805087dc28
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-06-20 18:22:22 +0200

    Create infodiscord6.sh

---
+++ Master/untserver/design/Info/infodiscord6.sh
@@ -0,0 +1,9 @@
+$HOME/script/discord/discord.sh \
+    --webhook-url "https://discord.com/api/webhooks/831649185757265962/_kehmOcmpxWdk5VtASrJ0gxQ3Ih1VK5d0uU3g82aa5lVnqmw4KhOUK9KGA8zRv1qn0Xb" \
+    --avatar "https://imgur.com/Ii4SoiK.png" \
+    --username "Pasta-Bot" \
+    --title "[EU] Pastanetwork #6 - Vanilla - Permanent" \
+    --url "https://www.battlemetrics.com/servers/unturned/11496961" \
+    --description "**IP:** \`ravioli.pastanetwork.eu\`\n**Port:** \`27020\`\n**Server location:** :flag_fr: Nanterre\n\n**Map:** Kuwait\n**Gamemode:** Survival\n**Type:** Vanilla\n**Reset:** Never\n\n**Vote:** https://unturned-servers.net/server/257856/vote/ \n**Statistics:** https://unturned.pastanetwork.eu/statistics/" \
+    --color "0x008000" \
+    --image "https://imgur.com/HiFJjhN.png" \
