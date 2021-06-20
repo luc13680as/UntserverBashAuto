587e5dbd876c769baf857251bc874597fa05a901
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-06-20 16:06:42 +0200

    Create messages.sh

---
+++ Node/autoannounce/messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -A message
+
+message[0]="You can join our discord at discord.unturned.pastanetwork.eu"
+message[1]="Thank you for playing on Pastanetwork !"
+message[2]="You can check our website at unturned.pastanetwork.eu"
+message[3]="The server is permanent - That mean no decaying and no wipe"
+message[4]="This server is Vanilla - No mods and no plugins"
+message[5]="Need any help ? Any suggestion ? Please use our discord"
+message[6]="You can vote for the next map to add on our network by joining our discord"
+message[7]="You can check your statistics on our website"
