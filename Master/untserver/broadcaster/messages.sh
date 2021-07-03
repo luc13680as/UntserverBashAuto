1a6ea8036fff6d2f9871afaa7d41264081ed2960
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-07-04 00:22:46 +0200

    Create messages.sh

---
+++ Master/untserver/broadcaster/messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -A message
+
+message[0]="Vote reward are coming soon !"
+message[1]="Thank you for playing on Pastanetwork !"
+message[2]="Our network restart everyday at 05:00 UTC+2"
+message[3]="The server is permanent - That mean no inventory or map reset"
+message[4]="This server is Vanilla - No mods and no plugins"
+message[5]="Any help ? Any suggestion ? Please use our discord"
+message[6]="You can vote for the next map to add on our network by joining our discord"
+message[7]="You can check your statistics on our website"
