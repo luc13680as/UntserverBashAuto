906c130aadca2b2586a118433a1f218eb1e0d00a
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-10-31 20:23:50 +0100

    Create infodiscord3.sh

---
+++ Master/discord/infodiscord3.sh
@@ -0,0 +1,9 @@
+$HOME/script/discord/discord.sh \
+    --webhook-url "https://discord.com/api/webhooks/831649185757265962/_kehmOcmpxWdk5VtASrJ0gxQ3Ih1VK5d0uU3g82aa5lVnqmw4KhOUK9KGA8zRv1qn0Xb" \
+    --avatar "https://imgur.com/Ii4SoiK.png" \
+    --username "Pasta-Bot" \
+    --title "[EU] Pastanetwork #3 - Vanilla - Permanent" \
+    --url "https://www.battlemetrics.com/servers/unturned/10464010" \
+    --description "**IP:** \`farfalle.pastanetwork.eu\`\n**Port:** \`27025\`\n**Server location:** :flag_fr: Nanterre\n\n**Map:** Elver\n**Gamemode:** Survival\n**Type:** Vanilla\n**Reset:** Never\n\n**Vote:** https://unturned-servers.net/server/252991/vote/ \n**Statistics:** https://unturned.pastanetwork.eu/statistics/" \
+    --color "0x008000" \
+    --image "https://imgur.com/HZzJIFb.png" \
