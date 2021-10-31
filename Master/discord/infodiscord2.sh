1a13fd110a5229453f1119d552b8da5d593b2b24
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-10-31 20:23:30 +0100

    Create infodiscord2.sh

---
+++ Master/discord/infodiscord2.sh
@@ -0,0 +1,9 @@
+$HOME/script/discord/discord.sh \
+    --webhook-url "https://discord.com/api/webhooks/831649185757265962/_kehmOcmpxWdk5VtASrJ0gxQ3Ih1VK5d0uU3g82aa5lVnqmw4KhOUK9KGA8zRv1qn0Xb" \
+    --avatar "https://imgur.com/Ii4SoiK.png" \
+    --username "Pasta-Bot" \
+    --title "[EU] Pastanetwork #2 - Vanilla - Permanent" \
+    --url "https://www.battlemetrics.com/servers/unturned/9777639" \
+    --description "**IP:** \`farfalle.pastanetwork.eu\`\n**Port:** \`27020\`\n**Server location:** :flag_fr: Nanterre\n\n**Map:** Russia\n**Gamemode:** Survival\n**Type:** Vanilla\n**Reset:** Never\n\n**Vote:** https://unturned-servers.net/server/252990/vote/ \n**Statistics:** https://unturned.pastanetwork.eu/statistics/" \
+    --color "0x008000" \
+    --image "https://imgur.com/TSuJbcH.png" \
