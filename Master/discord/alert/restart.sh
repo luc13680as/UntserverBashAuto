3a58627447d8b0f8de3bac631c318afe6b3d19a2
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-10-31 20:39:36 +0100

    Create restart.sh

---
+++ Master/discord/alert/restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+avatarimg="https://imgur.com/Ii4SoiK.png"
+img="https://imgur.com/gAJqceG.png"
+
+./discord.sh --avatar "$avatarimg" --username "Pasta-Bot" --color 0xFFAA00 --image "$img"
