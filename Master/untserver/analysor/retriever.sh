54b193eac1634dae07f408a9e6f3d4f5eb320173
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-11-14 00:35:25 +0100

    Update retriever.sh

--- Master/untserver/analysor/retriever.sh
+++ Master/untserver/analysor/retriever.sh
@@ -30,22 +30,29 @@ do
 	hostnametrunc=$(echo "$hostname" | cut -d. -f1)
 	(
     ssh untserver@"${hostname}" "[[ -d $logbackupdir ]] || mkdir -p $logbackupdir";
+    [[ -d $logbackupdir ]] || mkdir -p $logbackupdir;
 
 	for server in ${hostservers["$hostnametrunc"]};
 	do
-		#Get the date of the file and apply it to the name
-		logdate=$(ssh untserver@"${hostname}" "date -r \$HOME/serverfiles/Logs/Server_${server}_Prev.log '+%Y-%m-%d-%H-%M'");
-		ssh untserver@"${hostname}" "mv \$HOME/serverfiles/Logs/Server_${server}_Prev.log $server-$logdate";
+		if ssh -q untserver@"${hostname}" "test /home/untserver/serverfiles/Logs/Server_${server}_Prev.log";
+		then
+			echo "File found for $server";
 
-        #Get the file and make a backup of it
-	    scp -rp untserver@"${hostname}":$HOME/serverfiles/Logs/$server-$logdate.log "$logsdir"/;
-	      #Distant copy
-        ssh untserver@"${hostname}" "mv \$HOME/serverfiles/Logs/$server-$logdate.log $logbackupdir/";
+		    #Get the date of the file and apply it to the name
+		    logdate=$(ssh untserver@"${hostname}" "date -r \$HOME/serverfiles/Logs/Server_${server}_Prev.log '+%Y-%m-%d-%H-%M'");
 
-          #Local copy
-        [[ -d $logbackupdir ]] || mkdir -p $logbackupdir
-        cp "$logsdir/$server-$logdate.log" $logbackupdir
+		    echo "Date set on $logdate for file $server";
 
+		    ssh untserver@"${hostname}" "mv \$HOME/serverfiles/Logs/Server_${server}_Prev.log \$HOME/serverfiles/Logs/$server-$logdate.log";
+
+            #Get the file and make a backup of it
+	        scp -rp untserver@"${hostname}":$HOME/serverfiles/Logs/$server-$logdate.log "$logsdir"/;
+	          #Distant copy
+            ssh untserver@"${hostname}" "mv \$HOME/serverfiles/Logs/$server-$logdate.log $logbackupdir/";
+
+              #Local copy
+            cp "$logsdir/$server-$logdate.log" $logbackupdir;
+        fi;
 	done) &
 
 	nodepidstop["hostnametrunc"]=$!
