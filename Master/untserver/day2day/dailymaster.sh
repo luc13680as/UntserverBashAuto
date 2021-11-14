e891d5b3941dedb96224c3da81411cd80818fa2f
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-11-14 22:15:54 +0100

    Update dailymaster.sh

--- Master/untserver/day2day/dailymaster.sh
+++ Master/untserver/day2day/dailymaster.sh
@@ -4,7 +4,7 @@
 #                                                             #
 # Name: DailyMaster                                           #
 # Description: Execute the daily routine on other hosts       #
-# Version: 1.0                                                #
+# Version: 1.2                                                #
 # Creator: luc13680as                                         #
 #                                                             #
 ###############################################################
@@ -15,25 +15,36 @@ source "$HOME"/script/servers.sh
 echo -e "Launching daily procedure !"
 
 #Set the offset to have the correct date on data
-date=$(date -d "yesterday" '+%Y-%m-%d-%H-%M')
-backupdir="/backup"
+date=$(date '+%Y-%m-%d-%H-%M')
+backupdir="$HOME/backup/servers"
+discorddir="$HOME/discord/alert"
+
+#Verifying if everything is here
+[[ -d $backupdir ]] || mkdir -p "$backupdir"
 
 echo -e "Backup date set on $date"
 
+echo -e "Sending discord notification"
+
 #Send that the network is restarting
-"$HOME"/script/discord/discord.sh --avatar "https://imgur.com/Ii4SoiK.png" --username "Pasta-Bot" --color 0xFFAA00 --image "https://imgur.com/gAJqceG.png"
+"$discorddir"/discord.sh --avatar "https://imgur.com/Ii4SoiK.png" --username "Pasta-Bot" --color 0xFFAA00 --image "https://imgur.com/gAJqceG.png"
+
+echo -e "Warning players of the restart !" 
 
 #Broadcasting and shutdown of servers on the list
-i=120
+i=300
 while [[ "$i" -ge 0 ]]
 do
+	echo -e "Restart in $i seconds !"
+
 	#Browsing node list
 	for hostname in $hostslist
 	do
 		#Browsing servers for each node
 		for server in ${hostservers["$(echo "$hostname" | cut -d. -f1)"]}
 		do
-			ssh untserver@"${hostname}" "tmux send-keys -t $server:0 Space Enter; tmux send-keys -t $server:0 \"broadcast FFAA00 Restart of the server in $i seconds\" Enter"
+			#Sending a message on each server from each node
+			ssh untserver@"${hostname}" "tmux send-keys -t $server:0 Space Enter; tmux send-keys -t $server:0 \"broadcast FFAA00 Restart of the server in $i seconds\" Enter" &
 		done
 	done
 
@@ -42,56 +53,51 @@ do
 
 	if [[ "$i" -eq 0 ]]
 	then
-		echo -e "Shutting down of servers"
+		echo -e "Servers will be shutdown now"
+
+        #Store each node's subprocess pid
+        declare -A nodepidstop
 
 		#Browsing node list
 		for hostname in $hostslist
 	    do
-	    	#Browsing servers for each node
-		    for server in ${hostservers["$(echo "$hostname" | cut -d. -f1)"]}
-		    do
-		    	echo "Shutdown of $server on $hostname"
-			    ssh untserver@"${hostname}" "\$HOME/$server stop"
-			    sleep 1
-		    done
 
-		    #Send that the node is down
-            #0xFF0000
+		    hostnametrunc=$(echo "$hostname" | cut -d. -f1)
+
+		    ssh untserver@"${hostname}" "[[ -d $backupdir ]] || mkdir -p $backupdir";
+
+            #Execute stop, backup commands in a subprocess
+            (
+            declare -A serverpidstop; 
+            	
+            for server in ${hostservers["$hostnametrunc"]}; 
+            do 
+            	ssh untserver@"${hostname}" "\$HOME/$server stop" & serverpidstop["$server"]=$!; 
+            done; 
+            
+            wait "${serverpidstop[@]}"; 
+            
+            echo "The node $hostnametrunc is now down"; 
+            
+            ssh untserver@"${hostname}" "tar -czvf $backupdir/$hostnametrunc-$date.tar.gz /home/untserver/serverfiles/Servers" & 
+            backuppid=$!;
+            
+            wait "$backuppid"; 
+            
+            scp -rp "untserver@${hostname}:$backupdir/$hostnametrunc-$date.tar.gz" "$backupdir/$hostnametrunc/" & 
+                
+            for server in ${hostservers["$hostnametrunc"]}; 
+            do 
+                ssh untserver@"${hostname}" "\$HOME/$server update-lgsm; \$HOME/$server start" & 
+            done; 
+                
+            echo "The node $hostnametrunc is now up") &
+            
+            nodepidstop["hostnametrunc"]=$!
             
 	    done
+	    wait "${nodepidstop[@]}"
 	fi
 done
 
-echo "Backup of each node"
-for hostname in $hostslist
-do
-	hostnametrunc=$(echo "$hostname" | cut -d. -f1)
 
-	#Making the backup
-	ssh untserver@"${hostname}" "tar -czvf /backup/$hostnametrunc-$date.tar.gz /home/untserver/"
-
-	for server in ${hostservers["$hostnametrunc"]}
-	do
-		#Copy the log file on local host
-		ssh untserver@"${hostname}" "cp \$HOME/serverfiles/Logs/Server_$server.log /backup/logs/$server-$date.log"
-		
-		#Retrieving logs file
-		scp -rpv untserver@"${hostname}":"\$HOME/serverfiles/Logs/Server_$server.log" "/home/untserver/script/sortor/data/$server-$date.log"
-
-        #Does not restart the server on a certain time of the week
-		if [[ ! $(date +%u) -eq 1 ]]
-		then
-			ssh untserver@"${hostname}" "\$HOME/$server update-lgsm; \$HOME/$server start"
-		fi
-
-	done
-
-	#Send that the node is up
-    #0x008000
-
-	#Transfert the backup here
-	scp -rpv "untserver@${hostname}:/backup/$hostnametrunc-$date.tar.gz" "/backup/servers/$hostnametrunc/"
-
-done
-
-echo "Done !"
