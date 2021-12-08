84ea2c5d963b8d44530e9eb32013fd0add011f43
luc13680as <81090640+luc13680as@users.noreply.github.com>
2021-12-08 18:13:45 +0100

    Update dailymaster.sh

--- Master/untserver/daily/dailymaster.sh
+++ Master/untserver/daily/dailymaster.sh
@@ -4,7 +4,7 @@
 #                                                             #
 # Name: DailyMaster                                           #
 # Description: Execute the daily routine on other hosts       #
-# Version: 1.3.1                                              #
+# Version: 1.4                                                #
 # Creator: luc13680as                                         #
 #                                                             #
 ###############################################################
@@ -37,7 +37,7 @@ echo -e "Sending discord notification"
 echo -e "Warning players of the restart !" 
 
 #Broadcasting and shutdown of servers on the list
-i=300
+i=30
 while [[ "$i" -ge 0 ]]
 do
 	echo -e "Restart in $i seconds !"
@@ -53,9 +53,6 @@ do
 		done
 	done
 
-	sleep 30
-	((i=i-30))
-
 	if [[ "$i" -eq 0 ]]
 	then
 		echo -e "Servers will be shutdown now"
@@ -69,31 +66,38 @@ do
 
 		    hostnametrunc=$(echo "$hostname" | cut -d. -f1)
 
+
+            echo "Checking the backup directory of $hostnametrunc"
 		    ssh untserver@"${hostname}" "[[ -d $serverbackupdir ]] || mkdir -p $serverbackupdir";
+		    ssh untserver@"${hostname}" "find $serverbackupdir -type f -name \"*.tar.gz\" -mtime +7 | xargs rm -f"
 
             #Execute stop, backup commands in a subprocess
             (
             declare -A serverpidstop; 
             	
             for server in ${hostservers["$hostnametrunc"]}; 
-            do 
-            	ssh untserver@"${hostname}" "\$HOME/$server stop" & 
+            do
+            	echo "Stoping server $server"
+            	ssh untserver@"${hostname}" "\$HOME/$server stop" > /dev/null & 
             	serverpidstop["$server"]=$!; 
             done; 
             
             wait "${serverpidstop[@]}"; 
             
             echo "The node $hostnametrunc is now down"; 
-            
-            ssh untserver@"${hostname}" "tar -czvf $serverbackupdir/$hostnametrunc-$date.tar.gz /home/untserver/serverfiles/Servers /home/untserver/lgsm/config-lgsm/untserver";
+
+            echo "Backup of the node $hostnametrunc"
+            ssh untserver@"${hostname}" "tar -czf $serverbackupdir/$hostnametrunc-$date.tar.gz /home/untserver/serverfiles/Servers /home/untserver/lgsm/config-lgsm/untserver";
 
             [[ -d $serverbackupdir/$hostnametrunc ]] || mkdir -p "$serverbackupdir/$hostnametrunc";
             
+            echo "Receiving backup from the node $hostnametrunc"
             scp -rp "untserver@${hostname}:$serverbackupdir/$hostnametrunc-$date.tar.gz" "$serverbackupdir/$hostnametrunc";
                 
             for server in ${hostservers["$hostnametrunc"]}; 
             do 
-                ssh untserver@"${hostname}" "\$HOME/$server update-lgsm; \$HOME/$server start" & 
+            	echo "Starting server $server"
+                ssh untserver@"${hostname}" "\$HOME/$server update-lgsm > /dev/null; \$HOME/$server start > /dev/null" & 
             done; 
                 
             echo "The node $hostnametrunc is now up") &
@@ -103,7 +107,16 @@ do
 	    done
 	    wait "${nodepidstop[@]}"
 	fi
+
+	sleep 30
+	((i=i-30))
 done
 
 #Send a backup with rclone on the cloud
+echo "sending backups to the cloud"
 rclone copy "$backupdir" "$rcloneremotename:/$rclonebucketname"
+
+echo "Removing old backup"
+find $serverbackupdir -type f -name "*.tar.gz" -mtime +30 | xargs rm -f
+
+echo "Done !"
