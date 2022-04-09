1f94f5ea697893644752313c8b17f657bca03fb3
luc13680as <81090640+luc13680as@users.noreply.github.com>
2022-04-09 15:20:00 +0200

    Create information.sh

---
+++ Master/discord/info/information.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#./template.sh [title] [titleurl] [ip] [port] [location] [map] [gamemode] [type] [reset] [vote] [stats] [image]
+
+#Pastanetwork #1
+./template.sh "[EU] Pastanetwork #1 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13608741" "farfalle.pastanetwork.eu" "27015"  ":flag_fr: Nice" "Germany" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/243657/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/UAlWxQq.png"
+sleep 1
+#Pastanetwork #2
+./template.sh "[EU] Pastanetwork #2 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13608733" "farfalle.pastanetwork.eu" "27020"  ":flag_fr: Nice" "Russia" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/252990/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/TSuJbcH.png"
+sleep 1
+#Pastanetwork #3
+./template.sh "[EU] Pastanetwork #3 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13608732" "farfalle.pastanetwork.eu" "27025"  ":flag_fr: Nice" "Elver" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/252991/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/HZzJIFb.png"
+sleep 1
+#Pastanetwork #4
+./template.sh "[EU] Pastanetwork #4 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13608739" "farfalle.pastanetwork.eu" "27030"  ":flag_fr: Nice" "France" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/252992/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/L8Mthgh.png"
+sleep 1
+#Pastanetwork #5
+./template.sh "[EU] Pastanetwork #5 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/11186211" "ravioli.pastanetwork.eu" "27015"  ":flag_fr: Nanterre" "Yukon" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/255816/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/6eGoNKj.png"
+sleep 1
+#Pastanetwork #6
+./template.sh "[EU] Pastanetwork #6 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/11496961" "ravioli.pastanetwork.eu" "27020"  ":flag_fr: Nanterre" "Kuwait" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/257856/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/HiFJjhN.png"
+sleep 1
+#Pastanetwork #7
+./template.sh "[EU] Pastanetwork #7 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/12047189" "ravioli.pastanetwork.eu" "27025"  ":flag_fr: Nanterre" "Hawaii" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/261683/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/y8S14iL.png"
+sleep 1
+#Pastanetwork #8
+./template.sh "[EU] Pastanetwork #8 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13457925" "ravioli.pastanetwork.eu" "27030"  ":flag_fr: Nanterre" "Washington" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/273942/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/8uQn19P.png"
+sleep 1
+#Pastanetwork #9
+./template.sh "[EU] Pastanetwork #9 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/13653709" "fusilli.pastanetwork.eu" "27015"  ":flag_fr: Nice" "Arid" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/275368/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/R7aAMhg.png"
+#Pastanetwork #10
+./template.sh "[EU] Pastanetwork #10 - Vanilla - Permanent" "https://www.battlemetrics.com/servers/unturned/14891794" "fusilli.pastanetwork.eu" "27020"  ":flag_fr: Nice" "PEI" "Survival" "Vanilla" "Never" "https://unturned-servers.net/server/285386/vote/" "https://unturned.pastanetwork.eu/statistics/" "https://imgur.com/MMlhRpa.png"
