ff16d7b8cdc77c3365c227c1176edc0dacbf5a0d
luc13680as <81090640+luc13680as@users.noreply.github.com>
2022-05-14 13:19:17 +0200

    Update daemonupdate.sh

--- Master/untserver/updater/daemonupdate.sh
+++ Master/untserver/updater/daemonupdate.sh
@@ -3,6 +3,6 @@ while true
 do
     rsstail -i 2 -u https://smartlydressedgames.com/rss/unturned-steam-dedicated-server-updates.xml -n 0 | while read line
     do 
-        /home/untserver/script/updater/updater.sh
+        /home/untserver/script/daily/dailymaster.sh update
     done
 done
